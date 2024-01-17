4c6903439fba530b3d288e55de297fa99c4d7950
karina cruz francisco <karinacek307@gmail.com>
2024-01-17 15:57:04 -0600

    primer ejercicio terminado

---
+++ EJERCICIOS/SQLQuery1.sql
@@ -0,0 +1,40 @@
+-- SQL-LDD
+-- crear la base de datos bdentornosvip
+
+create database bdentornosvip
+
+go
+-- cambair de base de datos 
+use bdentornosvip
+go 
+
+-- crea una tabla categoria 
+create table tblcategoria(
+categoriaId int not null,
+descripcion varchar(100) not null,
+constraint pk_tblcategoria
+primary key (categoriaId),
+unique (descripcion)
+)
+go 
+
+-- crear la tabla productos 
+CREATE TABLE tblProducto(
+  productoId int not null,
+  nombreP varchar(50) not null,
+  precio decimal(10,2) not null,
+  existencia int not null,
+  categoriaId int not null,
+  constraint pk_tblProducto
+  primary key(productoId),
+  constraint unique_nombreP
+  unique(nombreP),
+  constraint chk_precio
+  check(precio>0.0 and precio<=10000),
+  constraint chk_existencia
+  check(existencia>=0),
+  constraint fk_tblProducto_tblcategoria
+  foreign key(categoriaId)
+  references tblcategoria(categoriaId)
+)
+drop table tblProducto
---
+++ EJERCICIOS/practica1.sql
@@ -0,0 +1,16 @@
+--prsctica #1
+ -- crear bd practica1
+create database bdpractica1
+
+go
+
+-- cambiar la base de datos 
+use bdpractica1
+go
+
+-- crear tabal de contacto
+create table tbcontacto(
+contactoId int not null,
+contacto varchar (60) not null, 
+
+)
